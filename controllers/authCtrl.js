73c8ee69bbe66cbfb078df1403ffcd58f80d1d95
Iasi Salomon <iasisalomon@gmail.com>
2024-09-02 02:01:44 -0300

    validation errors user model

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -1,5 +1,15 @@
 const User = require("../models/Users");
 
+//handle errors
+const handleErrors = (err) => {
+    const errors = [];
+    // checking validation
+    if (err.name === "ValidationError") {
+        Object.values(err.errors).map((val) => errors.push(val.message));
+    }
+    return { errors: errors };
+};
+
 module.exports.signup_get = (req, res) => {
     res.render("signup");
 };
@@ -11,13 +21,14 @@ module.exports.login_get = (req, res) => {
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const user = User.create({
+        const user = await User.create({
             email: email,
             password: password,
         });
         res.status(201).json(user);
     } catch (error) {
-        res.status(400).json(error);
+        const errors = handleErrors(error);
+        res.status(400).json(errors);
     }
 };
 
--- models/Users.js
+++ models/Users.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
+const isEmailValid = require("../helpers/emailValidator");
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, "email is required"],
         unique: true,
+        lowercase: true,
+        validate: [isEmailValid, "please enter a valid email"],
     },
     password: {
         type: String,
-        required: true,
-        minLength: 3,
+        required: [true, "email is required"],
+        minlength: [3, "password is too short"],
     },
 });
 
